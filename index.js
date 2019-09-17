9b148ae278f0e0f64350f22cc514aa34e53a3e17
Benoit Marchant <marchant@mac.com>
2019-09-16 17:11:15 -0700

    Updates Q.master implementation

--- index.js
+++ index.js
@@ -166,18 +166,6 @@ Q.getBluebirdPromise = function() {
 
 
 /**
- * sends a message to a value in a future turn
- * @param object* the recipient
- * @param op the name of the message operation, e.g., "when",
- * @param args further arguments to be forwarded to the operation
- * @returns result {Promise} a promise for the result of the operation
- */
-Q.dispatch = dispatch;
-function dispatch(object, op, args) {
-    return Q(object).dispatch(op, args);
-}
-
-/**
  * Annotates an object such that it will never be
  * transferred away from this process over any promise
  * communication channel.
@@ -188,13 +176,9 @@ function dispatch(object, op, args) {
  */
 Q.master = master;
 function master(object) {
-    return Promise({
-        "isDef": function () {}
-    }, function fallback(op, args) {
-        return dispatch(object, op, args);
-    }, function () {
-        return Q(object).inspect();
-    });
+    var promise = Promise.resolve(object);
+    promise.isDef = function () {};
+    return promise;
 }
 
 Promise.prototype.valueOf = Promise.prototype.value;
